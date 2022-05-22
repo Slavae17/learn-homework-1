4efa5dd28f56b776e683b33e96fac1ccdb094d62
Вячеслав Ершов <slavae17@mail.ru>
2022-05-22 16:58:23 +0300

    my HW about except

--- 6_exception1.py
+++ 6_exception1.py
@@ -13,8 +13,19 @@
 def hello_user():
     """
     Замените pass на ваш код
+    
     """
-    pass
+try:
+    user_says = input('Как дела? ')
+    while user_says != "Хорошо":
+        user_says = input('Как дела? ')
+          
+except (KeyboardInterrupt):
+    print(" Пока! ")
+
+# не знаю куда сунуть брейк но программа и так завершается
+    
+        
     
 if __name__ == "__main__":
     hello_user()
